e515ea8018100200fff0f2b1a5feaafd4f6a4f2e
Yang Ning <steven_nyang@outlook.com>
2016-03-30 22:51:24 +0800

    Refined.

---
+++ Development/AppGene/AppGene.Common.Entities.Infrastructure/Inferences/ModelNameGetter.cs
@@ -0,0 +1,45 @@
+﻿using AppGene.Common.Entities.Infrastructure.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace AppGene.Common.Entities.Infrastructure.Inferences
+{
+    public class ModelNameGetter
+    {
+
+        public EntityAnalysisContext Context { get; set; }
+
+        /// <summary>
+        /// Gets the entity name.
+        /// </summary>
+        /// <param name="context">The entity analysis context.</param>
+        /// <returns>The display entity name.</returns>
+        public virtual String GetName(EntityAnalysisContext context)
+        {
+            Context = context;
+
+            var modelAttribute = context.EntityType.GetCustomAttribute<ModelAttribute>();
+            var name = GetEntityName(context.EntityType, modelAttribute);
+
+            return name;
+        }
+
+        private string GetEntityName(Type entityType, ModelAttribute modelAttribute)
+        {
+            if (modelAttribute == null)
+            {
+                return EntityAnalysisHelper.GetPropertyDisplayName(entityType.Name);
+            }
+            else if (modelAttribute.ResourceType == null)
+            {
+                return modelAttribute.Name;
+            }
+
+            return EntityAnalysisHelper.GetResourceString(modelAttribute.ResourceType, modelAttribute.Name);
+        }
+    }
+}
